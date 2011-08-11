36fa1c6b724fd8f7bd05793e27e9eff2a569acef
William J Bowman <wilbowma@indiana.edu>
2011-08-11 17:13:35 -0400

    Oh yeeaaa this is beautiful. Working concurrency/chanel implemented with real primitives.

--- mpi_concurrency.hs
+++ mpi_concurrency.hs
@@ -1,35 +1,36 @@
-import Control.Monad.Reader
-import Control.Concurrent.Chan
 import Control.Concurrent hiding (yield)
 import Control.Monad.Cont
 import qualified Control.Concurrent as CC (yield)
+import Control.Monad.Reader
 import Debug.Trace
 
+-- I use the continuation monad here, not for concurrency, but for to
+-- allow me to wrap final answers in a list. This further
+-- allows me to do other things more easily, and hopefully allows for
+-- easier abstraction later. 
 type Comp a = Reader (Chan Int) a
 
-data Action r = Res r
-              | Susp (() -> Comp (Action r))
-              | Par (Comp (Action r)) (Comp (Action r))
+type Proc r a = Cont (ReaderT (Chan Int) IO [r]) a
 
-type Proc r a = Cont (Comp (Action r)) a
-
-par :: Proc r a -> Proc r a -> Proc r a
-par (Cont c1) (Cont c2) = Cont (\k -> Par (c1 k) (c2 k))
+par :: Show r => Proc r a -> Proc r a -> Proc r a
+par (Cont c1) (Cont c2) = Cont (\k ->
+  do ch <- ask
+     liftIO $ runPar (runReaderT (c1 k) ch) (runReaderT (c2 k) ch))
 
 yield :: Proc r ()
-yield = Cont (\k -> (Susp k))
+yield = Cont (\k -> do liftIO $ CC.yield; k ())
 
 send :: Int -> Proc r ()
 send s = Cont (\k -> do ch <- ask
-                        put (ch ++ [s])
+                        liftIO $ writeChan ch s
                         k ())
 
 recv :: Proc r Int
-recv = Cont (\k -> do ch <- get
-                      case ch of
-                        [] -> runCont (do yield; recv) k
-                        (s:ss) -> do put ss; k s)
-
+recv = Cont (\k -> do ch <- ask
+                      -- Since we're using real threads, I shouldn't
+                      -- need to manually yield here.
+                      i <- liftIO $ readChan ch
+                      k i)
 wrapProc :: Show r => IO [r] -> MVar () -> MVar [r] -> IO ()
 wrapProc p block mvar = do
   xs <- p
@@ -54,33 +55,34 @@ waitOnChildren children = do
         takeMVar x
         waitOnChildren children
 
-runPar :: Show r => Action r -> Action r -> IO [r]
+runPar :: Show r => IO [r] -> IO [r] -> IO [r]
 runPar k1 k2 = do
   children <- newMVar []
   results <- newMVar []
-  forkChild results children $ wrapProc (sched k1)
-  forkChild results children $ wrapProc (sched k2)
+  forkChild results children $ wrapProc k1
+  forkChild results children $ wrapProc k2
   -- Block until all children have returned
   waitOnChildren children
   res <- takeMVar results
   return res
 
-sched :: Show r => Action r -> IO [r]
-sched x = 
-  case x of 
-       (Res r) -> return [r]
-       (Susp k) -> do CC.yield; sched $ k ()
-       (Par k1 k2) -> runPar k1 k2
-
 runProc :: Show r => Proc r r -> IO [r]
-runProc p = sched $ runCont p (\r -> (Res r))
+runProc p = do
+  ch <- newChan
+  runReaderT (runCont p (\r -> return [r])) ch
 
-
+test1 :: Proc Int Int
+test1 = (par (return 1) (return 2))
+test2 :: Proc Int Int
+test2 = (par (do yield; (par (return 1) (return 2)))
+             (return 3))
+test3 :: Proc Int Int
 test3 = (par
          (do send 2
              return 0)
          (do x <- recv
              return (x+1)))
+test4 :: Proc Int Int
 test4 = (foldr1 par
                   [do x <- recv; send (x+1); return 0,
                    return 1,
