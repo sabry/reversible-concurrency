7ef9765fa59ef7eb133eaab5bc26d641d679c1ee
William J Bowman <wilbowma@indiana.edu>
2012-01-09 21:39:32 -0500

    New attempt at semantics.. failed attempt
    Work on channel. Mostly abstraction to make specifying preds 1 line.

--- Reversible/Channel2.hs
+++ Reversible/Channel2.hs
@@ -33,39 +33,66 @@ module Reversible.Channel2 (
 
   data ChannelState =
       Consistent 
-    | SendRequest 
-    | RecvRequest
-    | CommAccept
-    | RecvBacktrack
-    | SendBacktrack
-    | BacktrackAccept 
+    | SendReq
+    | SendInt1
+    | SendBack
+    | SendBInt1
+    | RecvAck
+    | CommAcc
+    | RecvBack
+    | BackAck
     deriving (Eq,Show,Read)
+  
+  predList :: _Channel a -> [Maybe ChannelState]
+  predList ch = [consist, sendReq, recvReq, commAcc, recvBack,
+                 sendBack, backAcc]
 
-  getState :: Channel a -> IO ChannelState
-  getState ch = do
-    ch <- readIORef ch
-    return $ case compare(sendTime ch, recvTime ch) of
-       -- 0,0,0 < Consistent
-       -- 1,0,1 < Comm accept
-       -- 0,1,0 < Backtrack accept
-      EQ -> case compare(sendTime ch, chanTime ch) of
-              -- 0,0,0 < Consistent
-            EQ -> Consistent
-              -- 1,0,1 < Comm accept
-            GT -> CommAccept
-              -- 0,1,0 < Backtrack accept
-            LT -> BacktrackAccept
-       -- 0,1,1 < Send backtrack emit
-       -- 0,0,1 < Receive request
-      LT -> if sendTime ch < chanTime ch
-             then SendBacktrack
-             else RecvRequest
-       -- 1,0,0 < Send request
-       -- 1,1,0 < Receiver backtrack emit
-      GT -> if sendTime ch > chanTime ch
-             then SendRequest
-             else RecvBacktrack
+  -- I can't believe something like this isn't built in
+  maybeIf :: Bool -> a -> Maybe a
+  maybeIf b s -> if b then Just s else Nothing
 
+  -- I think I just abstracted too much
+  type ChanRel = Time -> Time -> Bool
+  pred :: ChanRel -- c ? r
+          -> ChanRel -- c ? s
+          -> ChanRel -- s ? r
+          -> ChannelState
+          -> _Channel a
+          -> Maybe ChannelState
+  pred ch r1 r2 r3 st = let 
+    c = chanTime ch
+    r = recvTime ch
+    s = sendTime ch in
+      maybeIf ((r1 c r) && (r2 c s) && (r3 s r)) st
+
+  top :: a -> a -> Bool
+  top _ _ = True
+
+  
+  -- they will all by :: _Channel a -> Maybe ChannelState
+  -- c ? r, c ? s, s ? r
+  consit = pred (==) (==) (==) Consistent
+  sendReq = pred (==) (<) top SendReq
+  -- s > c, r > s
+  sendInt1 ch = pred top (<) (<) SendInt1
+  -- c < s, s = r
+  recvAck ch = pred top (<) (==)
+  -- s < c, c = r
+  sendBack ch = pred (==) (>) top
+  -- r < c, s < c, r < s
+  sendBInt1 ch = pred (>) (>) (>)
+    
+  getState :: _Channel a -> ChannelState
+  getState ch = let ls = dropWhile (\(a,_) -> a) $ 
+    map (\f -> case f ch of
+                Nothing -> (False, undefined)
+                Just st -> (True, st)) predList 
+    case len ls of
+      1 -> head ls
+      a | a > 1 -> error "This channel is in more than 1 state: " ++
+        (show ls)
+      a | a == 0 -> error "This channel is not in a state!"
+      _ -> error "Appearently numbers don't work"
   
   _putSendTime :: Time -> _Channel a -> _Channel a
   _putSendTime time ch = _Channel {chanValue ch, time, chanTime ch, recvTime ch}
@@ -79,30 +106,6 @@ module Reversible.Channel2 (
   _putChanValue :: a -> _Channel a -> _Channel a
   _putChanValue val ch = _Channel {val, sendTime ch, chanTime ch, recvTime ch}
 
-  putSendTime :: Time -> Channel a -> IO ()
-  putSendTime time = (flip atomicModifyIORef) $ (\ch -> (_putSendTime time ch, ()))
-
-  putRecvTime :: Time -> Channel a -> IO ()
-  putRecvTime time = (flip atomicModifyIORef) $ (\ch -> (_putRecvTime time ch, ()))
-
-  putChanTime :: Time -> Channel a -> IO ()
-  putChanTime time = (flip atomicModifyIORef) $ (\ch -> (_putChanTime time ch, ()))
-
-  putChanValue :: a -> Channel a -> IO ()
-  putChanValue val = (flip atomicModifyIORef) $ (\ch -> (_putChanValue val ch, ()))
-
-  getSendTime :: Channel a -> IO Time
-  getSendTime = sendTime <$> readIORef 
-
-  getRecvTime :: Channel a -> IO Time
-  getRecvTime = recvTime <$> readIORef
-
-  getChanTime :: Channel a -> IO Time
-  getChanTime = chanTime <$> readIORef
-
-  getChanValue :: Channel a -> IO a
-  getChanValue = chanValue <$> readIORef
-
   newChannel :: a -> IO (Channel a)
   newChannel a = newIORef Channel {
     chanValue = a,
@@ -113,82 +116,26 @@ module Reversible.Channel2 (
 
   newEmptyChannel :: IO (Channel a)
   newEmptyChannel =  newChannel undefined
-
-  sendBacktrack :: Time -> Channel a -> IO () 
-  sendBacktrack time ch = do
-    cTime <- getChanTime
-    if (not $ time < cTime)
-      -- A sendBacktrack must put the send time before the current channel
-      -- time
-      then error "Error in sendBacktrack: given time is not" ++
-        " less than the channel time"
-      else putSendTime time ch
-
-  recvBacktrack :: Time -> Channel a -> IO ()
-  recvBacktrack time ch = do
-    cTime <- getChanTime
-    if (not $ time < cTime)
-      -- A recvBacktrack must put the recv time before the current channel
-      -- time
-      then error "Error in recvBacktrack: given time is not" ++
-        " less than the channel time"
-      else putRecvTime time ch
   
+  atomicModifyChannel :: Channel a -> states -> 
+                        (_Channel a -> (_Channel a, b)) ->  IO b
+  atomicModifyChannel ch states f = atomicModifyIOREf (\_chan ->
+    if elem (getState _chan) states
+      then f _chan
+      else error "Invalid precondition")
 
-  -- Given a current state and a state we want to be in, return a
-  -- function that will move to that state.
-  transition :: ChannelState -> ChannelState -> Channel a -> Channel a
-  transition cur tar = 
-    let bad = error "Invalid transition: " ++ (show cur) ++ " -> " ++ (show tar)
-    in
-    case cur of
-      Consistent ->
-        case tar of 
-          SendRequest 
-          RecvRequest
-          RecvBacktrack
-          SendBacktrack
-          _ -> bad
-
-      SendRequest -> 
-        case tar of
-          CommAccept
-          RecvBacktrack
-          _ -> bad
-
-      RecvRequest ->
-        case tar of 
-          CommAccept
-          SendBacktrack
-          _ -> bad
-
-      CommAccept ->
-        case tar of 
-          Consistent 
-            -- since transitioning to consistent is the senders job, the
-            -- receiver might immediately make a request
-          RecvBacktrack
-          RecvRequest
-          _ -> bad
-
-      RecvBacktrack ->
-        case tar of
-          BacktrackAccept 
-          _ -> bad
-
-      SendBacktrack ->
-        case tar of
-          BacktrackAccept 
-          _ -> bad
-
-      BacktrackAccept ->
-       case tar of 
-          Consistent 
-            -- since transitioning to consistent is the
-            -- senders job, the receiver might immediately make a request
-          RecvRequest 
-          RecvBacktrack
-          _ -> bad
+  send :: Time -> a -> Channel a -> IO Time
+  send time ch val = do 
+    _ <- atomicModifyChannel ch sendStates (\_chan ->
+      (_putChanValue val (_putSendTime time ch), ()))
+    -- Now we must wait until the state is changed. We need some sort of
+    -- busy-loop over atomicModifyChannel.
+    fix (\f -> 
+      atomicModifyChannel (\_chan ->
+        
+      )
+    )
+    
 
   send :: Time -> Channel a -> a -> IO Time
   send time ch val = do
