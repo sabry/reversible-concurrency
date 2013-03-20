04681ab9454e38a60171e6d8432b9d84e8e690ee
toddaaro <abtodd@opprobrio.us>
2013-03-20 14:09:47 -0400

    a misnamed file with some type signatures

---
+++ akka/src/main/scala/RProcess.scala
@@ -0,0 +1,25 @@
+
+trait TimeDirection
+case object TForward extends TimeDirection
+case object TBackward extends TimeDirection
+
+trait MessageDirection
+case object SendMode extends MessageDirection
+case object ReceiveMode extends MessageDirection
+
+case class Channel(sender: ActorRef, receiver: ActorRef)
+
+case class ChannelView(raw: Channel, role: MessageDirection) {
+  var time = 0
+  var timeDirection: TimeDirection = TForward
+}
+
+case class Message(raw: Any, time: Int)
+
+class ChannelMap {
+  var raw = new Set[ChannelView]
+}
+
+case class Frame[Input,Output](time: Int, channelMap: ChannelMap, context: FooContext[Input,Output], value: Input) 
+
+case class FooContext[Input,Output]
